aaa1a06ab6bff7ed3581448ec4472b2c88f255fc
Peperon-s <126762601+Peperon-s@users.noreply.github.com>
2024-03-25 02:54:07 +0900

    Delete 1-youtube.py
    必要のないファイルであるため

--- 1-youtube.py
+++
@@ -1,9 +0,0 @@
-from yt_dlp import YoutubeDL
-print('動画url')
-url = input()
-
-
-ydl_opts = {'outtmpl':'J:/musicgame/down_load_video/%(title)s.mp4',
-            'format': 'bestvideo',}
-with YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
